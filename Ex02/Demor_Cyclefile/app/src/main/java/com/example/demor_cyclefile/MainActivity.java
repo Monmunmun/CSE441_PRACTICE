aee69ee5ca581e2e04887697890f2aa71487a9dc
Monmunmun <imubslayer4@gmail.com>
2024-09-28 17:12:24 +0700

    fixex02

--- Ex02/Demor_Cyclefile/app/src/main/java/com/example/demor_cyclefile/MainActivity.java
+++ Ex02/Demor_Cyclefile/app/src/main/java/com/example/demor_cyclefile/MainActivity.java
@@ -29,4 +29,34 @@ Button btncall;
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 }

d9813d41faf86276880c49edaa6bf978e310dffb
Monmunmun <imubslayer4@gmail.com>
2024-10-20 15:05:55 +0700

    fixex07_1

---
+++ Ex07/OpenChildActivity/app/src/main/java/com/example/openchildactivity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.openchildactivity;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button btn1 = findViewById(R.id.button1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, child_activity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
